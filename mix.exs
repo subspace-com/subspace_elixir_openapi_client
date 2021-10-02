e8746284a2bc8e0ee39d563685063548b0c96eef
Nathan Moore <nmoore@subspace.com>
2021-10-01 17:02:16 -0700

    Updated to version 1.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SubspaceProductAPI.Mixfile do
 
   def project do
     [app: :subspace_openapi_client,
-     version: "1.0",
+     version: "1.0.3",
      elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
